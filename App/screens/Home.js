2fd44f66e14e104d94501f1b2985cf7610f332ec
Waqar Ahmad <ahmadwaqar@outlook.com>
2022-03-29 19:19:21 +0500

    Added eventemitter3

--- App/screens/Home.js
+++ App/screens/Home.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import {Dimensions, StyleSheet, View} from 'react-native';
-import {Button} from 'react-native-paper';
-
-import EventEmitter from 'react-native/Libraries/vendor/emitter/EventEmitter';
 
 import AppBarComponent from '../components/AppBarComponent';
 import CarouselCard from '../components/homeScreen/CarouselCards';
@@ -11,20 +8,11 @@ import News from '../components/homeScreen/News';
 import colors from '../constants/colors';
 
 const Home = ({navigation, route}) => {
-  const newEvent = new EventEmitter();
-
-  newEvent.addListener('pressing', () => {
-    console.log('pressing');
-  });
-
   return (
     <View style={styles.container}>
       <AppBarComponent navigation={navigation} route={route} />
       <CarouselCard navigation={navigation} />
       <News navigation={navigation} />
-      <Button mode="contained" onPress={() => newEvent.emit('pressing')}>
-        Press me
-      </Button>
     </View>
   );
 }; // end of Home
